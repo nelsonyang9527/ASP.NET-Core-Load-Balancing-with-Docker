cac00353c17d3ed8b8bded0ef1ecff2624958596
NelsonYang <abc77101500@gmail.com>
2020-04-19 23:49:12 +0800

    初稿

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ASP.NET-Core-Load-Balancing-with-Docker
+
+使用 ASP.NET Core application with RESTful API
+使用 Docker and NGINX as reverse proxy 實現 Load balancing
+
+```
+docker-compose build
+docker-compose up -d --scale core-app=4 --no-recreate
+```
+
+注意: 由於 Nginx 免費版限制, Load balancing 最多可配置4個節點.
---
+++ dotnetapp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Models;
+
+namespace dotnetapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Machine"] = Environment.MachineName;
+            
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
