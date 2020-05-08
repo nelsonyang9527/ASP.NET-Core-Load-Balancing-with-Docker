fa0fbf810778bcd28752007a46380beeaf5115af
NelsonYang <abc77101500@gmail.com>
2020-05-08 14:39:40 +0800

    加入Web API

---
+++ dotnetapp/Controllers/APIController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnetapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APIController : ControllerBase
+    {
+        private static readonly string[] DataList = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<APIController> _logger;
+
+        public APIController(ILogger<APIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DataModel> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new DataModel
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = DataList[rng.Next(DataList.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    public class DataModel
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
